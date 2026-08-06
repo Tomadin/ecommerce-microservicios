2c6fc8a08879663b60cf0a62f31a38b97aed96ba
Brian Tomadin <tomadinbrian@gmail.com>
2026-08-06 19:23:19 -0300

    chore: scaffold inicial de los microservicios

---
+++ eureka-server/src/test/java/com/tomadin/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomadin.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
